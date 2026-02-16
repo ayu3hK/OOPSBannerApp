1e2b534415de37b519826afb1eee799181578982
ayushkunwar1 <kunwarayush85@gmail.com>
2026-02-16 15:53:55 +0530

    String join method

---
+++ UC3.java
@@ -0,0 +1,16 @@
+public class UC3 {
+     public static void main(String[] args) {
+
+        String banner = String.join("\n",
+            " *****   *****   *****   ***** ",
+            "**   ** **   ** **   ** **     ",
+            "**   ** **   ** **   ** **     ",
+            "**   ** **   ** *****   *****  ",
+            "**   ** **   ** **           **",
+            "**   ** **   ** **     **    **",
+            " *****   *****   **      ***** "
+        );
+
+        System.out.println(banner);
+    }
+}
