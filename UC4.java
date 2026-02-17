3c4d9b4618cd1ecbe6198bb9c00ec698b253bdd4
gautu1801 <gm8088@srmist.edu.in>
2026-02-17 22:28:03 +0530

    Updated Banner with array and for loop.

--- UC4.java
+++ UC4.java
@@ -1,7 +1,7 @@
 public class UC4 {
     public static void main(String[] args) {
 
-        // Construct banner lines and store in array
+        
         String[] banner = {
             " *****   *****   *****   ***** ",
             "**   ** **   ** **   ** **     ",
@@ -12,11 +12,8 @@ public class UC4 {
             " *****   *****   **      ***** "
         };
 
-        // Loop through array and print each line
         for (String line : banner) {
             System.out.println(line);
         }
-
-        // Program exits automatically after main ends
     }
 }
